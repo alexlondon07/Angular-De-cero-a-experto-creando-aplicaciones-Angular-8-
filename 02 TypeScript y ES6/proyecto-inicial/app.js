3cd59ef2b7ec746507f7ffb4da3b81b41593ef94
Alexander <alexander.londono@grupokonecta.com>
2019-08-16 14:11:10 -0500

    TypeScript

--- 02 TypeScript y ES6/proyecto-inicial/app.js
+++ 02 TypeScript y ES6/proyecto-inicial/app.js
@@ -1,7 +1,25 @@
-"use strict";
-function saludar(nombre) {
-    console.log("Hola " + nombre.toUpperCase());
+//Let and const
+var msj = "Hello";
+if (true) {
+    var msj_1 = "adios";
 }
-var wolwerine = {
-    nombre: "Logan"
+console.log(msj);
+var OPTIONS = "All";
+if (true) {
+    var OPTIONS_1 = "Nothings";
+}
+//TypeScript
+var my_name = "Alex";
+var num = 123;
+var boolean = true;
+var today = new Date();
+today = new Date('2019-08-16');
+var anyThings;
+anyThings = my_name;
+anyThings = num;
+anyThings = boolean;
+var spiderman = {
+    name: "Alex",
+    age: 28
 };
+console.log(today);
--- 02 TypeScript y ES6/proyecto-inicial/app.ts
+++ 02 TypeScript y ES6/proyecto-inicial/app.ts
@@ -1,7 +1,32 @@
-function saludar( nombre: string){
-    console.log("Hola " + nombre.toUpperCase() );
+
+//Let and const
+let msj = "Hello";
+if(true){
+    let msj = "adios";
+}
+console.log(msj);
+
+const OPTIONS = "All";
+if ( true ){
+    const OPTIONS = "Nothings";
 }
 
-var wolwerine = {
-    nombre: "Logan"
+
+//TypeScript
+let my_name:string = "Alex";
+let num:number = 123;
+let boolean:boolean = true;
+let today:Date = new Date();
+today = new Date('2019-08-16');
+let anyThings:any;
+
+anyThings = my_name;
+anyThings = num;
+anyThings = boolean;
+
+let spiderman = {
+    name: "Alex",
+    age: 28
 }
+
+console.log( today );
