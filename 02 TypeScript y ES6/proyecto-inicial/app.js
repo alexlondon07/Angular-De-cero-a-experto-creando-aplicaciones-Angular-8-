4e446e6eb06bc30ab8727a26a6d3631f9da5569b
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-08-18 14:44:04 -0500

    TypeScript y ES6

--- 02 TypeScript y ES6/proyecto-inicial/app.js
+++ 02 TypeScript y ES6/proyecto-inicial/app.js
@@ -37,8 +37,90 @@ function activate(who, obj, moment) {
     if (obj === void 0) { obj = "Batiseñal"; }
     var msj;
     if (moment) {
+        msj = who + " activ\u00F3 the " + obj + " en la " + moment;
+    }
+    else {
         msj = who + " activ\u00F3 the " + obj;
     }
     console.log(msj);
 }
-activate("Gordon");
+activate("Gordon", "Batiseñal", "Tarde");
+//Funciones de flechas  y funciones normal
+var myFunction = function (a) {
+    return a;
+};
+var myFunctionFlecha = function (a) { return a; };
+console.log(myFunction("Normal"));
+console.log(myFunction("Flecha"));
+var myFunction2 = function (a, b) {
+    return a + b;
+};
+var myFunction2Flecha = function (a, b) { return a + b; };
+var myFunction3 = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+var hulk = {
+    name: "Hulk",
+    smash: function () {
+        var _this = this;
+        setTimeout(function () { return console.log(_this.name + " smash!!"); }, 1500);
+    }
+};
+hulk.smash();
+//Destructuración de Objetos
+var avenger = {
+    nombre: "Steve",
+    clave: "Capitan America",
+    poder: "Droga"
+};
+var alias = avenger.nombre, clave = avenger.clave, poder = avenger.poder;
+console.log(alias, clave, poder);
+//Destructuración de Arreglos
+var avengers = ["Thor", "Steve", "Tony"];
+var thor = avengers[0], capi = avengers[1], iroman = avengers[2];
+console.log(thor, capi, iroman);
+//Promesas en ES6 - Ejecutar procesos asincronos
+var promise1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("Promesa terminado");
+        //Termina bien
+        resolve();
+        //Termina Mal∫
+        // reject();
+    }, 1500);
+});
+console.log("Paso 1");
+promise1.then(function () {
+    console.log("Ejecutarme cuando se termine bien!");
+}, function () {
+    console.log("Ejecutarme cuando se termine mal!");
+});
+console.log("Paso 2");
+function enviarMision(xmen) {
+    console.log("Enviando a : " + xmen.nombre);
+}
+function enviarCuartel(xmen) {
+    console.log("Enviando al cuartel : " + xmen.nombre);
+}
+var wolverine = {
+    nombre: "Wolerine",
+    poder: "Regeneración"
+};
+enviarMision(wolverine);
+enviarCuartel(wolverine);
+//Introduccion a las Clases de la POO
+var Avenger = (function () {
+    function Avenger(nombre, equipo, nombreReal) {
+        this.nombre = "Sin Nombre";
+        this.puedePelar = false;
+        this.peleasGanadas = 0;
+        console.log("Se ejecutó el constructor");
+        this.nombre = nombre;
+        this.equipo = equipo;
+        this.nombreReal = nombreReal;
+    }
+    return Avenger;
+}());
+var antMan = new Avenger("AntMan", "Cap", "Scott Lang");
+console.log(antMan);
--- 02 TypeScript y ES6/proyecto-inicial/app.ts
+++ 02 TypeScript y ES6/proyecto-inicial/app.ts
@@ -44,7 +44,6 @@ console.log( text2 );
 console.log( spiderman.name );
 
 //Funciones: Parámetros opcionales, obligatorios y por defecto
-
 function activate (
     who:string, 
     obj:string="Batiseñal", 
@@ -52,12 +51,127 @@ function activate (
     ){
 
     let msj:string;
-
     if(moment){
+        msj =  `${who} activó the ${obj} en la ${moment}` ;
+    }else{
         msj =  `${who} activó the ${obj}` ;
     }
-   
     console.log(msj);
 }
+activate("Gordon", "Batiseñal", "Tarde");
 
-activate("Gordon");
+//Funciones de flechas  y funciones normal
+
+let myFunction = function (a:string){
+    return a;
+}
+
+let myFunctionFlecha = a =>a;
+
+console.log(myFunction("Normal"));
+console.log(myFunction("Flecha"));
+
+
+let myFunction2 = function (a:number, b:number){
+    return a + b;
+}
+let myFunction2Flecha = ( a:number, b:number)=> a + b;
+
+let myFunction3 = function (nombre:string){
+    nombre = nombre.toUpperCase();
+    return nombre;
+}
+
+let hulk = {
+    name: "Hulk",
+    smash(){
+        setTimeout( () => console.log( this.name + " smash!!"), 1500);
+    }
+}
+hulk.smash();
+
+//Destructuración de Objetos
+
+let avenger = {
+    nombre: "Steve",
+    clave: "Capitan America",
+    poder: "Droga"
+}
+let { nombre:alias, clave, poder } = avenger;
+console.log( alias, clave, poder );
+
+//Destructuración de Arreglos
+
+let avengers:string[] = ["Thor", "Steve", "Tony"];
+let [ thor, capi, iroman ] = avengers;
+
+console.log( thor, capi, iroman );
+
+//Promesas en ES6 - Ejecutar procesos asincronos
+let promise1 = new Promise( function(resolve, reject ){
+    setTimeout(() => {
+        console.log("Promesa terminado");
+        //Termina bien
+        resolve();
+
+        //Termina Mal∫
+       // reject();
+
+    }, 1500);
+})
+
+console.log("Paso 1");
+
+promise1.then( function(){
+    console.log("Ejecutarme cuando se termine bien!")
+},
+    function(){
+        console.log("Ejecutarme cuando se termine mal!")
+    }
+)
+console.log("Paso 2");
+
+
+//Interfaces in TypeScript
+interface Xmen {
+    nombre: string,
+    poder: string
+}
+
+function enviarMision ( xmen : Xmen){
+    console.log("Enviando a : " + xmen.nombre);
+}
+
+function enviarCuartel ( xmen : Xmen){
+    console.log("Enviando al cuartel : " + xmen.nombre);
+}
+
+
+let wolverine:Xmen = {
+    nombre: "Wolerine",
+    poder: "Regeneración"
+}
+
+enviarMision( wolverine );
+enviarCuartel( wolverine );
+
+//Introduccion a las Clases de la POO
+
+
+class Avenger {
+    nombre: string = "Sin Nombre";
+    equipo:string;
+    nombreReal:string;
+    puedePelar:boolean = false;
+    peleasGanadas:number = 0;
+
+    constructor(nombre:string, equipo:string, nombreReal:string){
+        console.log("Se ejecutó el constructor");
+        this.nombre = nombre;
+        this.equipo =equipo;
+        this.nombreReal = nombreReal;
+    }
+}
+
+let antMan:Avenger = new Avenger("AntMan", "Cap", "Scott Lang");
+console.log(antMan);
