e0cac2069c6fd439f9614af3fdacc8a95de02ab3
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-08-19 14:44:30 -0500

    Iniciando con la sessión 04 del curso, SPA

---
+++ 04 Aplicación SPA/spa/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+//Components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+//Routes
+import { APP_ROUTING } from './app.routes';
+import { HeroeService } from './services/heroes.service';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
+//Services
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    APP_ROUTING,
+  ],
+  providers: [ HeroeService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 04 Aplicación SPA/spa/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'heroe/:id', component: HeroeComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+]
+
+//export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
