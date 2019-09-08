5fd3f30ba651860c283766ac253f66c4f2764e0a
Alexander Londoño Espejo <alexlondon07@gmail.com>
2019-09-08 15:47:03 -0500

    Componente del héroe

--- 04 Aplicación SPA/spa/src/app/components/heroe/heroe.component.ts
+++ 04 Aplicación SPA/spa/src/app/components/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroeService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe:any = {};
+
+  constructor( 
+    private activateRoute: ActivatedRoute,
+    private _heroeService: HeroeService
+    ) {
+    
+    this.activateRoute.params.subscribe ( params => {
+      this.heroe =this._heroeService.getHeroe(params['id']);
+    });
+  }
 
   ngOnInit() {
   }
